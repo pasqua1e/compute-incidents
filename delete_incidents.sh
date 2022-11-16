64240df31ac3cea6411cc8f1513266101ef9d3ab
Pasquale <31853954+pasqua1e@users.noreply.github.com>
2022-11-16 15:41:38 +0000

    Add files via upload

---
+++ delete_incidents.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+#user="" -- this is the access key generated in Prisma Cloud
+#pass="" -- Secret key in Prisma Cloud
+compute_url="https://europe-west3.cloud.twistlock.com/eu-2-143543850"
+#remember that compute_url is the value found in Compute->System->Tools "path to console"
+#so it's in the form https://europe-west3.cloud.twistlock.com/eu-2-12345678
+
+
+
+
+accept="Accept: application/json"
+
+usage() { 
+	echo "This script is used to delete all the incidents"
+    echo "in the Compute->Monitor->Incidents screen" 
+	echo -e "\nMake sure you define the following variables:"
+    echo "- user"
+    echo "- pass"
+    echo "- compute_url" 
+	} 
+# if less than two arguments supplied, display usage 
+ 
+if [ -z "${user}" ] || [ -z "${pass}" ] || [ -z "${compute_url}" ]
+then
+    usage
+    exit 1
+fi
+echo "OK"
+exit 0
+delete_incidents () {
+    curl -k -X PATCH -H "$accept" -u "$user:$pass" "$compute_url/api/v1/audits/incidents/acknowledge/$1?project=Central+Console" --data-raw '{"acknowledged":true}'
+    #echo "$compute_url/api/v1/audits/incidents/acknowledge/$1?project=Central+Console"
+}
+incidents=`curl -k -H "$accept" -u "$user:$pass" "$compute_url/api/v1/audits/incidents?acknowledged=false" |jq '.[] | ."_id"'`
+#remove the double quotes from the output
+incidents=`echo $incidents |sed s/\"//g`
+#echo $incidents
+
+for i in $incidents; do delete_incidents $i;done
